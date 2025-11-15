97a6e08b5c42789bb0327c75157c095c0ec00cd4
Brian Abbott <burnedfaceless@gmail.com>
2025-11-15 10:47:22 -0500

    added retrofit dependancy

--- app/src/main/java/activities/MainActivity.java
+++ app/src/main/java/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package edu.georgiasouthern.weatherly;
+package edu.georgiasouthern.weatherly.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import edu.georgiasouthern.weatherly.R;
 
 public class MainActivity extends AppCompatActivity {
 
